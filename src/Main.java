30616e5a64c9ae56e9f95ff0acd2cc60f0e7b78e
Sergei US <106164110+QA-USV@users.noreply.github.com>
2022-06-23 12:39:50 +0300

    FirstCommit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 public class Main {
-
     public static void main(String[] args) {
 
-        int currentSum = 150;
-        int sumToAdd = 1200;
+        int currentSum = 120;
+        int sumToAdd = 1590;
         int sumToAddToGetBonus = 1000;
         int bonusForEachSum = 100;
         int bonus = 1;
         boolean rightToGetBonus = sumToAdd > sumToAddToGetBonus;
         int bonusTotal = rightToGetBonus ? sumToAdd / bonusForEachSum * bonus : 0;
         int balance = currentSum + sumToAdd + bonusTotal;
-
         System.out.println("Спасибо за пополнение счета на " + sumToAdd + " рублей!");
         System.out.println("Вам начислено " + bonusTotal + " бонусных рублей.");
         System.out.println("Баланс Вашего счета " + balance + " рублей.");
-
     }
-
 }
